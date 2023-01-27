29562828e3a9011f1f6ad3cc497ae7c5b3fa7adc
Souvik Mandal <Souvik.Mandal@clinisys.com>
2023-01-27 16:12:00 +0530

    first commit

---
+++ If_Else/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program{
+    static void Main(string[] args){
+        Console.Write("Enter your marks: ");
+        int marks=Convert.ToInt32(Console.ReadLine());
+        string grade;
+        if(marks>=95){
+            grade= "AA";
+            Console.WriteLine("You passed with good marks");
+            Console.WriteLine("Your grade is: "+grade);
+        }
+        else if(marks>=85){
+            grade= "A";
+            Console.WriteLine("You passed with good marks");
+            Console.WriteLine("Your grade is: "+grade);
+        }
+        else if(marks>=30){
+            grade= "B";
+            Console.WriteLine("You passed moderate marks");
+            Console.WriteLine("Your grade is: "+grade);
+        }
+        else{
+            grade= "F";
+            Console.WriteLine("You failed");
+            Console.WriteLine("Your grade is: "+grade);
+        }
+
+
+    }
+}
---
+++ While loop/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+class WhileExample{
+    static void Main(){
+int i=9;
+while(i>=0){
+    System.Console.Write(i+" ");
+    i--;
+}
+System.Console.WriteLine();
+
+int j=0;
+do{
+    System.Console.Write(j+" ");
+    j++;
+}while(j<10);
+System.Console.WriteLine();
+}
+}
+
