f1a46225382cfded8b30ed6af9838e256fbe5742
Souvik Mandal <Souvik.Mandal@clinisys.com>
2023-01-30 15:00:22 +0530

    Override

---
+++ Method_Overriding/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+
+
+public class Home {
+	
+	
+	string name = "Ground Floor";
+
+	
+	public virtual void showdata()
+	{
+		Console.WriteLine("Floor Name: " + name);
+	}
+}
+
+
+class Member : Home {
+	
+	
+	int s = 10;
+	
+	
+	public override void showdata()
+	{
+		
+		
+		base.showdata();
+		
+		Console.WriteLine("No of members: " + s);
+	}
+}
+
+class Run {
+	
+	
+	static void Main()
+	{
+		
+		
+		Member E = new Member();
+		
+		
+		
+		E.showdata();
+		
+	}
+}
