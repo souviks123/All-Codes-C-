0d9bb43b20989fbbb9fe77372332c25379668763
Souvik Mandal <Souvik.Mandal@clinisys.com>
2023-01-30 15:12:57 +0530

    Goto

---
+++ Goto/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Goto{
+    static void Main(){
+        int i=1;
+        System.Console.WriteLine("India");
+        System.Console.WriteLine("Australia");
+        goto mylabel;
+        System.Console.WriteLine("Africa");
+        System.Console.WriteLine("US");
+        mylabel:
+        System.Console.WriteLine("UK");
+        System.Console.WriteLine("Bangladesh");
+
+    }
+}
