d4f74d60c48be0525f7047914ea6ead284f2ea15
Souvik Mandal <Souvik.Mandal@clinisys.com>
2023-01-31 14:55:21 +0530

    Asynchronous

---
+++ Asynchronous/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Asynchronous{
+    internal class Test{
+        static void Main(string[] args){
+            Task1();
+            Task2();
+            Task3();
+            Console.ReadLine();
+        }
+        public static async void Task1(){
+            await Task.Run(()=>{
+                System.Console.WriteLine("Task 1 Starting.."); 
+                Thread.Sleep(1000);
+                System.Console.WriteLine("Task 1 Completed");
+            });
+            
+        }
+
+        public static async void Task2(){
+            await Task.Run(()=>{
+                System.Console.WriteLine("Task 2 Starting.."); 
+                Thread.Sleep(2000);
+                System.Console.WriteLine("Task 2 Completed");
+            });
+            
+        }
+
+        public static async void Task3(){
+            await Task.Run(()=>{
+                System.Console.WriteLine("Task 3 Starting.."); 
+                Thread.Sleep(3000);
+                System.Console.WriteLine("Task 3 Completed");
+            });
+            
+        }
+    }
+}
