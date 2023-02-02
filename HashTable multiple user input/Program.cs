3f6762dc66a9a51268612432f1dc83b2e11c061d
Souvik Mandal <Souvik.Mandal@clinisys.com>
2023-02-02 11:32:07 +0530

    hashtable with unlimited inputs

---
+++ HashTable multiple user input/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+namespace hash{
+    class Test{
+        static void Main(string[] args)
+        {
+            Hashtable h1=new Hashtable();
+            while(true){
+                System.Console.WriteLine("Enter key or 00 to exit ");
+                var s=Console.ReadLine()!;
+                if(s=="00"){
+                    break;
+                }else{
+                    System.Console.WriteLine("Enter value");
+                    var p=Console.ReadLine();
+                    h1.Add(s,p);
+                }
+
+
+            }
+            System.Console.WriteLine("-----------------------------------");
+            foreach(DictionaryEntry i in h1){
+                System.Console.WriteLine($"The key: {i.Key} and the value: {i.Value}");
+            }
+            System.Console.WriteLine("Thank you");
+        }
+    }
+}
