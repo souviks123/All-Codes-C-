20e0f1e3218a7ec307706b05c26a70f57e0154ee
Souvik Mandal <Souvik.Mandal@clinisys.com>
2023-02-06 11:11:01 +0530

    Stack

---
+++ Stack/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections;
+
+class StackDemo {
+
+	
+	static public void Main()
+	{
+
+		
+		Stack my_stack = new Stack();
+
+		
+		my_stack.Push("Souvik");
+		my_stack.Push("Mandal");
+		my_stack.Push('M');
+		my_stack.Push(null);
+		my_stack.Push(1234);
+		my_stack.Push(580);
+         
+
+		
+		foreach(var elem in my_stack)
+		{
+			Console.WriteLine(elem);
+		}
+	}
+}
