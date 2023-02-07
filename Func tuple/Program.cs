11067953cae1f826a5cad6370121c7eef78ffa80
Souvik Mandal <Souvik.Mandal@clinisys.com>
2023-02-07 16:15:13 +0530

    Func using tuple

---
+++ Func tuple/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Tuple{
+    class Program{
+        static void Main(string[] args)
+        {
+            Func<(int,int,int),(int,int,int)> Double=(x)=>(x.Item1*2,x.Item2*2,x.Item3*2);
+            System.Console.WriteLine(Double((1,2,3)));
+            
+            Func<(int,int,int),(int,int,int)> cube=(x)=>(x.Item1*x.Item1*x.Item1,x.Item2*x.Item2*x.Item2,x.Item3*x.Item3*x.Item3);
+            System.Console.WriteLine(cube((4,5,6)));
+
+        }
+    }
+}
