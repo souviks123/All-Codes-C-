1d96095790eb023f2a32d7b1ec5fc60a71bebde5
Souvik Mandal <Souvik.Mandal@clinisys.com>
2023-02-08 10:52:16 +0530

    Tuple using Action

---
+++ Action tuple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Actiontuple
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            
+            Action<(int,string,string)>s=(x)=>Console.WriteLine($"Id = {x.Item1},\nFirst name = {x.Item2},\nLast name = {x.Item3}");
+            System.Console.WriteLine("Enter your id");
+            int id=Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Enter your first name");
+            string name=Console.ReadLine()!;
+            System.Console.WriteLine("Enter your last name");
+            string lname=Console.ReadLine()!;
+            s((id,name,lname));
+            
+
+        }
+
+        
+    }
+}
